451b53b5c97176db418e03a35e73bd9ac5a974f0
Ramon Brand <ramon2009@gmail.com>
2020-05-28 12:43:23 +0300

    Add descriptions to first two locations

--- locations/BackRoom.js
+++ locations/BackRoom.js
@@ -1,6 +1,13 @@
 import Location from '../Location.js'
 
 export default class BackRoom extends Location {
+  name = 'Glorod\'s Back Room'
+  description = {
+    sight: 'The oak walls look as thought they have seen better days... rot creeping in perhaps... the woods looks weak, and the book shelves look as though they might be hiding something sinister...',
+    smell: 'Oaky and moldy',
+    hearing: 'Quiet... with just the distant low rumble of the drinkers outside the door...'
+  }
+  locations = ['Bar']
   onEnter() {
     this.on("listen", () => {
       console.log("There are strange creeking noises coming from the walls...")
--- locations/Bar.js
+++ locations/Bar.js
@@ -2,6 +2,12 @@ import Location from '../Location.js'
 
 export default class Bar extends Location {
   name = 'Glorod\'s Mystic Bar'
+  description = {
+    sight: 'A rustic old bar, there are paintings on the dark oak walls, seemingly all of a man... a man with a beard and a chiseled jaw... there seems to be a door leading to a back room...',
+    smell: 'Oaky and that of alcohol...',
+    hearing: 'Glasses clanking and a bar man... just... speaking into thin air... as though days roll by slow'
+  }
+  locations = ['BackRoom']
   onEnter() {
     this.on("drink", () => {
       console.log("Goin for a drink")
